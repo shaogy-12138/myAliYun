607a333b6dfecb33073018521d5abad98ccbac8f
shaogy-12138 <shaogy12138@163.com>
2022-01-20 11:56:06 +0800

    add c/readme.c and add linux/readme.c

--- Linux/readme.c
+++ Linux/readme.c
@@ -1,14 +1,13 @@
 /*************************************************************************
-	> File Name: c/readme.c
+	> File Name: Linux/readme.c
 	> Author: Jason.shao
 	> Mail: 
-	> Created Time: Thu Jan 20 11:34:06 2022
+	> Created Time: Thu Jan 20 11:54:18 2022
  ************************************************************************/
 
 #include<stdio.h>
 
 int main(){
-    printf("this is a test file\n");
-    printf("first time modify this file\n");
+    printf("this is Linux study file\n");
     return 0;
 }
