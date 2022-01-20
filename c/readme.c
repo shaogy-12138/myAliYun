55cb7ec2e81dbbe54e4631e3d10811029f66e7f0
shaogy-12138 <shaogy12138@163.com>
2022-01-20 11:35:01 +0800

    add a file

---
+++ c/readme.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: c/readme.c
+	> Author: Jason.shao
+	> Mail: 
+	> Created Time: Thu Jan 20 11:34:06 2022
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(){
+    printf("this is a test file\n");
+    return 0;
+}
